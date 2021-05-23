04b33834c84e84c0c2ac3a1b2d82bbf81396e2fb
ryma1738 <51891293+ryma1738@users.noreply.github.com>
2021-05-23 16:14:04 -0600

    Added the date picker to the inputs

--- assets/js/script.js
+++ assets/js/script.js
@@ -89,10 +89,17 @@ $(".list-group").on("click", "span", function() {
 
   $(this).replaceWith(dateInput);
 
+  dateInput.datepicker({
+    minDate: 1,
+    onClose: function() {
+      $(this).trigger("change");
+    }
+  });
+
   dateInput.trigger("focus");
 });
 
-$(".list-group").on("blur", "input[type='text']", function() {
+$(".list-group").on("change", "input[type='text']", function() {
   var date = $(this).val().trim();
 
   var status = $(this)
@@ -153,6 +160,10 @@ $("#trash").droppable({
   drop: function(event, ui) {
     ui.draggable.remove();
   }
+});
+
+$("#modalDueDate").datepicker({
+  minDate: 1
 });
 
 
